72460d468d3f37bf324de75eb0bbd65a375ae6bd
Zachary Hale <zachary.hale88@gmail.com>
2019-08-04 12:42:28 -0400

    Adding the column actually.  EF got a little weird

---
+++ Migrations/20190804164111_AddMeasureNumber2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewOnline.Migrations
+{
+    public partial class AddMeasureNumber2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "measureNumber",
+                table: "Measure",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "measureNumber",
+                table: "Measure");
+        }
+    }
+}
