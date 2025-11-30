f16ea0cb32ba846441f560457f921546d2fe528e
Zachary Hale <zachary.hale@vistrue.com>
2025-11-29 22:47:13 -0500

    update to Angular 19

--- metronome.client/src/app/app.component.ts
+++ metronome.client/src/app/app.component.ts
@@ -9,9 +9,10 @@ interface WeatherForecast {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone: false
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
