4690630d4aa9971868b34a935897232c73558fd0
Zachary Hale <zachary.hale@vistrue.com>
2025-11-29 22:41:12 -0500

    Redo on this

--- ClientApp/src/app/constants/global.const.ts
+++
@@ -1,3 +0,0 @@
-export class Constants {
-    public static localJWTKey: string = 'authKey';
-}
--- ClientApp/src/app/models/composer.model.ts
+++
@@ -1,6 +0,0 @@
-export class Composer {
-    id: string;
-    first_name: string;
-    last_name: string;
-    birth_date: string;
-}
