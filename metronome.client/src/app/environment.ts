fd088e42a8fa35eed99e94d1324a07c719607fae
Zachary Hale <zachary.hale@vistrue.com>
2025-12-07 19:53:41 -0500

    Add GitHub action workflow for the front-end

--- metronome.client/src/app/environment.ts
+++ metronome.client/src/app/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7270'
+  production: false,
+  //apiUrl: 'https://localhost:7270',
+  apiUrl: 'http://138.197.78.98:5000',
 };
